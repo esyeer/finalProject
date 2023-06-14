5edc3973ae1cb9e9cc74c80ee11ca8473eb17a33
ACER <ACER@LAPTOP-CJR7DT6D>
2023-06-14 22:52:56 +0800

    CodingID Project Final

---
+++ Scripts/Mobile/TS-Mobile-003- Update Profile/TCUP007-Update BirthDate 6 Years Old/Script1686752435170.groovy
@@ -0,0 +1,60 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\ACER\\Videos\\Demo_APP_CodingID_1.0.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/Mobile/Login/btn_loginHere'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Mobile/Login/field_mail'), 'faceluck04256@gmail.com', 0)
+
+Mobile.setText(findTestObject('Object Repository/Mobile/Login/field_pass'), '@Qwerty123', 0)
+
+Mobile.delay(2)
+
+Mobile.tap(findTestObject('Object Repository/Mobile/Login/Button_Login'), 0)
+
+//Mobile.delay(2)
+Mobile.tap(findTestObject('Object Repository/Mobile/Update Profile/Button_Profile'), 0)
+
+//Mobile.delay(2)
+Mobile.tap(findTestObject('Object Repository/Mobile/Update Profile/Gear_Icon'), 0)
+
+//Mobile.delay(2)
+Mobile.tap(findTestObject('Object Repository/Mobile/Update Profile/Edit_Profile'), 0)
+
+//Mobile.delay(2)
+Mobile.setText(findTestObject('Object Repository/Mobile/Update Profile/Field_Name'), 'syarif hidayatullah', 0)
+
+//Mobile.delay(2)
+Mobile.setText(findTestObject('Object Repository/Mobile/Update Profile/Field_Phone'), '082189913645', 0)
+
+//Mobile.delay(2)
+Mobile.setText(findTestObject('Object Repository/Mobile/Update Profile/Field_Birthdate'), '13-Nov-2017', 0)
+
+Mobile.pressBack()
+//Mobile.delay(2)
+//Mobile.tap(findTestObject('Object Repository/Mobile/Update Profile/Button_Save Changes'), 0)
+//
+////Mobile.delay(2)
+//Mobile.tap(findTestObject('Object Repository/Mobile/Update Profile/btn_popUp_Okay'), 0)
+
+Mobile.delay(1)
+
+Mobile.closeApplication()
+
