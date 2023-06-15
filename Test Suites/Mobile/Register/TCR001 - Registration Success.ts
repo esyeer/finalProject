e3d236b0f28496fc1ce993117d5ed64224863471
dokcilboyz <dokcil.cool@gmail.com>
2023-06-15 22:00:35 +0700

    Update

---
+++ Test Suites/Mobile/Register/TCR001 - Registration Success.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TCR001 - Registration Success</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>580c551f-2454-4ee0-972c-0e6bb249eede</testSuiteGuid>
+   <testCaseLink>
+      <guid>dccb11a9-8c13-4d5c-a621-ee73bc8dd385</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/TS-Mobile-001- Register/Positive/TCR001 - Registration Success</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>e0ea35e1-6048-4bd1-94a3-270e6e46ccea</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>1-1</value>
+         </iterationEntity>
+         <testDataId>Data Files/Website-RegisterSuccess</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>e0ea35e1-6048-4bd1-94a3-270e6e46ccea</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>f1e438b5-d305-4842-a3f1-afb9d4548d44</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
